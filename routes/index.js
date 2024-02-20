98877ded46d6c60cebcf599a755fe8c33dedd5d3
Khushpreet Singh <75271300+khushpreet-007@users.noreply.github.com>
2024-02-21 00:12:12 +0530

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,83 @@ var router = express.Router();
 const localStrategy = require('passport-local');
 const userModel = require('G:/Project/Pinterest/pin/routes/users');
 const postModel = require('G:/Project/Pinterest/pin/routes/posts');
-
-
-
 const passport = require('passport');
 const upload = require('./multer');
 const { post } = require('../app');
 passport.use(new localStrategy(userModel.authenticate()));
+
+
+// chat gpt
+const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require('@google/generative-ai');
+const dotenv = require('dotenv').config()
+
+const MODEL_NAME = "gemini-pro";
+const API_KEY = process.env.API_KEY;
+
+
+async function runChat(userInput) {
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+  const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 1000,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    // ... other safety settings
+  ];
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "You are Khushpreet, a friendly assistant." }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Hello! Welcome to Home. My name is khushpreet. What's your name?" }],
+      },
+      {
+        role: "user",
+        parts: [{ text: "Hi" }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Hi there! Thanks for reaching out here ask me anything. Before I can answer your question, . Can you please provide more information?" }],
+      },
+    ],
+  });
+
+  const result = await chat.sendMessage(userInput);
+  const response = result.response;
+  return response.text();
+}
+
+router.post('/chat', async (req, res) => {
+  try {
+    const userInput = req.body?.userInput;
+    console.log('incoming /chat req', userInput)
+    if (!userInput) {
+      return res.status(400).json({ error: 'Invalid request body' });
+    }
+
+    const response = await runChat(userInput);
+    res.json({ response });
+  } catch (error) {
+    console.error('Error in chat endpoint:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
